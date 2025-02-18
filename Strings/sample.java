ef2d6698271bbc6de324a92b7b9456861c9e2f2e
sanjay nalamasa <sanjaynalamasa07@gmail.com>
2025-02-18 08:07:15 +0530

    ignore test

---
+++ Strings/sample.java
@@ -0,0 +1,12 @@
+public class sample {
+    public static void Num(int n) {
+        for(int i=0; i<=n; i++) {
+            System.out.println(i+" sahil i love you man");
+        }
+    }
+    public static void main(String args[]) {
+        System.out.println("Sahil Bro");
+        Num(1000);
+    }
+    
+}
